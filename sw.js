eca0de78dcdc565131bc3e5ddf95235ddca6a8b3
Aleksander Józefiak <aleksander.jozefiak@gmail.com>
2026-04-12 14:24:59 +0200

    add icons and fix path

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'statsborger-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'statsborger-v2';
+const ASSETS = [
+  '/joy-statsborger-quiz/',
+  '/joy-statsborger-quiz/index.html',
+  '/joy-statsborger-quiz/manifest.json',
+  '/joy-statsborger-quiz/icon-192.png',
+  '/joy-statsborger-quiz/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
